baa502e24e634a737c82722bc4ca38accbfa8946
ChangminYi <jerry9803@gmail.com>
2021-05-25 18:18:39 +0900

    Store Information showing activity added

--- SubGroup1_Store/Store/app/src/main/java/com/example/store/StoreInfoActivity.java
+++ SubGroup1_Store/Store/app/src/main/java/com/example/store/StoreInfoActivity.java
@@ -3,12 +3,27 @@ package com.example.store;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class StoreInfoActivity extends AppCompatActivity {
+    private TextView storeName, openTime, closeTime, location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store_info);
+
+        //matching textview with xml
+        this.storeName = (TextView) findViewById(R.id.textView_storeName);
+        this.openTime = (TextView) findViewById(R.id.textView_opentime);
+        this.closeTime = (TextView) findViewById(R.id.textView_closetime);
+        this.location = (TextView) findViewById(R.id.textView_location);
+
+        //getiing store info and synchronizing with textview
+        StoreInfo si = new StoreInfoAccessor().getStoreInfo();
+        this.storeName.setText(si.getName());
+        this.openTime.setText(si.getOpentime());
+        this.closeTime.setText(si.getClosetime());
+        this.location.setText(si.getLocation());
     }
 }
